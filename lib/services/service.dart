7a2a1372e45d24d23e20cd865ac76f692299f327
Jud-e <lenuanwa@gmail.com>
2022-11-11 16:38:19 -0800

    commit

--- lib/services/service.dart
+++ lib/services/service.dart
@@ -2,6 +2,8 @@ import 'package:country_details/model/country_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+bool _isSorted = false;
+
 class Service {
   Future<List<CountriesModel>> getCountries() async {
     var client = http.Client();
@@ -12,6 +14,7 @@ class Service {
       final json1 = json.decode(response.body);
       final List<CountriesModel> services =
           json1.map<CountriesModel>((e) => CountriesModel.fromJson(e)).toList();
+      services.sort((a, b) => a.name!.common!.compareTo(b.name!.common!));
       return services;
     } else {
       throw Exception("Failed to load");
