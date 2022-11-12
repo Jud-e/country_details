c1e4bc08aae73d82a62a997e2cb6a1ba1026ad21
Jud-e <lenuanwa@gmail.com>
2022-11-11 17:45:55 -0800

    commit

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,8 @@
 import 'package:country_details/model/country_model.dart';
 import 'package:country_details/services/service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -26,53 +28,72 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder<List<CountriesModel>>(
-            future: countries,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
+        body: SafeArea(
+      minimum: EdgeInsets.symmetric(horizontal: 24),
+      child: FutureBuilder<List<CountriesModel>>(
+          future: countries,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            if (snapshot.hasData) {
+              if (snapshot.data == null) {
+                return const Text("List is empty");
               }
-              if (snapshot.hasData) {
-                if (snapshot.data == null) {
-                  return const Text("List is empty");
-                }
-                return Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        children: [
-                          const SizedBox(
-                            height: 10.0,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text("data"),
-                            ],
-                          ),
-                          SizedBox(),
-                          Row(),
-                          SizedBox(),
-                          Row(),
-                        ],
+              return Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Explore",
+                        style: GoogleFonts.elsieSwashCaps(
+                            fontSize: 30, fontWeight: FontWeight.w700),
                       ),
-                    ),
-                    Flexible(
-                      child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: snapshot.data?.length,
-                          itemBuilder: (context, index) {
-                            return Text(
-                                "${snapshot.data?[index].name?.common}");
-                          }),
-                    ),
-                  ],
-                );
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-              return const Text("erroe");
-            }));
+                      const Icon(CupertinoIcons.sun_max)
+                    ],
+                  ),
+                  Flexible(
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (context, index) {
+                          return Row(
+                            children: [
+                              Image.network(
+                                "${snapshot.data?[index].flags?.png}",
+                                width: 40,
+                                height: 40,
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    "${snapshot.data?[index].name?.common}",
+                                    style: TextStyle(
+                                        fontFamily: "Axiforma",
+                                        fontWeight: FontWeight.w900),
+                                  ),
+                                  Text(
+                                    snapshot.data?[index].capital != null
+                                        ? "${snapshot.data?[index].capital[0]}"
+                                        : "",
+                                    style: TextStyle(
+                                        fontFamily: "Axiforma",
+                                        fontWeight: FontWeight.w500),
+                                  )
+                                ],
+                              ),
+                            ],
+                          );
+                        }),
+                  ),
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return const Text("erroe");
+          }),
+    ));
   }
 }
