2251976d4e835307c6b36d76aea9012d06870fa7
Jud-e <lenuanwa@gmail.com>
2022-11-14 09:05:11 -0800

    edits

--- lib/services/widget.dart
+++ lib/services/widget.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 bool value = false;
 bool showDialog = false;
-List<int> searchList = [];
+List searchList = [];
 List continents = [
   "Africa",
   "Europe",
@@ -31,97 +32,122 @@ class _ContainsState extends State<Contains> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: MediaQuery.of(context).viewInsets,
-      child: Wrap(
-        children: [
-          SizedBox(
-            child: Container(
-              width: double.maxFinite,
-              padding: const EdgeInsets.all(10),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
+    return Theme(
+      data: Theme.of(context),
+      child: Padding(
+        padding: MediaQuery.of(context).viewInsets,
+        child: Wrap(
+          children: [
+            SizedBox(
+              child: Container(
+                width: double.maxFinite,
+                padding: const EdgeInsets.all(10),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Filter",
-                          style: TextStyle(
-                              fontFamily: "Axiforma",
-                              fontWeight: FontWeight.w900),
-                        ),
-                        IconButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: const Icon(Icons.cancel))
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Continent",
-                          style: TextStyle(
-                              fontFamily: "Axiforma",
-                              fontWeight: FontWeight.w500),
-                        ),
-                        IconButton(
-                            onPressed: () {
-                              setState(() {
-                                showDialog = !showDialog;
-                              });
-                            },
-                            icon: const Icon(Icons.keyboard_arrow_down))
-                      ],
-                    ),
-                    Visibility(
-                      visible: showDialog,
-                      child: Column(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          ...List.generate(continents.length, (i) {
-                            return CheckboxListTile(
-                                value: values[i],
-                                title: Text("${continents[i]}"),
-                                onChanged: ((bool? newValue) {
-                                  setState(() {
-                                    values[i] = newValue!;
-                                  });
-                                }));
-                          })
+                          const Text(
+                            "Filter",
+                            style: TextStyle(
+                                fontFamily: "Axiforma",
+                                fontWeight: FontWeight.w900),
+                          ),
+                          IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: const Icon(Icons.cancel))
                         ],
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Timezone",
-                          style: TextStyle(
-                              fontFamily: "Axiforma",
-                              fontWeight: FontWeight.w500),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "Continent",
+                            style: TextStyle(
+                                fontFamily: "Axiforma",
+                                fontWeight: FontWeight.w500),
+                          ),
+                          IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  showDialog = !showDialog;
+                                });
+                              },
+                              icon: const Icon(Icons.keyboard_arrow_down))
+                        ],
+                      ),
+                      Visibility(
+                        visible: showDialog,
+                        child: Column(
+                          children: [
+                            ...List.generate(continents.length, (i) {
+                              return CheckboxListTile(
+                                  value: values[i],
+                                  title: Text("${continents[i]}"),
+                                  onChanged: ((bool? newValue) {
+                                    setState(() {
+                                      values[i] = newValue!;
+                                      if (values[i] == true) {
+                                        searchList.add(continents[i]);
+                                        print(searchList);
+                                      } else {
+                                        searchList.remove(continents[i]);
+                                        print(searchList);
+                                      }
+                                    });
+                                  }));
+                            }),
+                          ],
                         ),
-                        IconButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: const Icon(Icons.keyboard_arrow_down))
-                      ],
-                    ),
-                  ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "Timezone",
+                            style: TextStyle(
+                                fontFamily: "Axiforma",
+                                fontWeight: FontWeight.w500),
+                          ),
+                          IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: const Icon(Icons.keyboard_arrow_down))
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(
+                              onPressed: () {
+                                for (var i = 0; i < values.length; i++) {
+                                  values[i] = false;
+                                  searchList = [];
+                                }
+                              },
+                              child: const Text("Reset")),
+                          ElevatedButton(
+                              onPressed: () {}, child: Text("Show Results"))
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
